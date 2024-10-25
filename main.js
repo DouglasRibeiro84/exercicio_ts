73ae09a93e09a42415ab2940e9f14873a8a5d1cf
doouglas <douglasribeiro084@gmail.com>
2024-10-25 17:04:19 -0300

    implementando numeros aleatorios

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 "use strict";
 const multiplicar = (numero1, numero2) => numero1 * numero2;
 const saudar = (nome) => "ola me chamo " + nome;
-const resultado = multiplicar(5, 10);
+class GeradorAleatorio {
+    constructor(min, max) {
+        this.min = min;
+        this.max = max;
+    }
+    gerarNumero() {
+        return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+    }
+}
+const gerador = new GeradorAleatorio(1, 20);
+const numeroAleatorio1 = gerador.gerarNumero();
+const numeroAleatorio2 = gerador.gerarNumero();
+const resultado = multiplicar(numeroAleatorio1, numeroAleatorio2);
 const pessoa = saudar('Jose');
 console.log(pessoa, "e o valor final é", resultado);
--- main.ts
+++ main.ts
@@ -1,6 +1,25 @@
 const multiplicar = (numero1: number, numero2:number): number => numero1 * numero2
 const saudar = (nome: string) => "ola me chamo "+ nome
 
-const resultado = multiplicar(5,10)
+class GeradorAleatorio {
+    private min: number;
+    private max: number;
+
+    constructor(min: number, max: number) {
+        this.min = min;
+        this.max = max;
+    }
+
+    gerarNumero(): number {
+        return Math.floor(Math.random() * (this.max - this.min + 1)) + this.min;
+    }
+}
+
+const gerador = new GeradorAleatorio(1, 20);
+
+const numeroAleatorio1 = gerador.gerarNumero();
+const numeroAleatorio2 = gerador.gerarNumero();
+
+const resultado = multiplicar(numeroAleatorio1, numeroAleatorio2)
 const pessoa =  saudar('Jose')
 console.log(pessoa, "e o valor final é", resultado)
