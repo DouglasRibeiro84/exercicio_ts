2edeec0807123d9b68dbd72b3fa019dfa1eea0ca
doouglas <douglasribeiro084@gmail.com>
2024-10-25 16:55:28 -0300

    final commit

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+const multiplicar = (numero1, numero2) => numero1 * numero2;
+const saudar = (nome) => "ola me chamo " + nome;
+const resultado = multiplicar(5, 10);
+const pessoa = saudar('Jose');
+console.log(pessoa, "e o valor final é", resultado);
---
+++ main.ts
@@ -0,0 +1,6 @@
+const multiplicar = (numero1: number, numero2:number): number => numero1 * numero2
+const saudar = (nome: string) => "ola me chamo "+ nome
+
+const resultado = multiplicar(5,10)
+const pessoa =  saudar('Jose')
+console.log(pessoa, "e o valor final é", resultado)
